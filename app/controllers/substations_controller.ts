27a03839da56aafd067ac78c2a059d903b18e293
BolotnikovMS <bolotnikovms@yandex.ru>
2024-02-01 21:40:13 +0500

    feat: Описаны методы

--- app/controllers/substations_controller.ts
+++ app/controllers/substations_controller.ts
@@ -1,11 +1,36 @@
+import Substation from '#models/substation'
+import SubstationService from '#services/substation_service'
+import { substationValidator } from '#validators/substation'
 import type { HttpContext } from '@adonisjs/core/http'
 
 export default class SubstationsController {
-  async index({ request, response }: HttpContext) {}
+  async index({ request, response }: HttpContext) {
+    const substations = await SubstationService.getSubstations(request)
 
-  async store({ request, response }: HttpContext) {}
+    return response.status(200).json(substations)
+  }
 
-  async update({ params, request, response }: HttpContext) {}
+  async store({ request, response }: HttpContext) {
+    const validatedData = await request.validateUsing(substationValidator)
+    const substation = await Substation.create({ userId: 1, ...validatedData })
 
-  async destroy({ params, response }: HttpContext) {}
+    return response.status(201).json(substation)
+  }
+
+  async update({ params, request, response }: HttpContext) {
+    const substation = await Substation.findOrFail(params.id)
+    const validatedData = await request.validateUsing(substationValidator)
+    const updDistrict = await substation.merge(validatedData).save()
+
+    return response.status(200).json(updDistrict)
+  }
+
+  async destroy({ params, response }: HttpContext) {
+    const substation = await Substation.findOrFail(params.id)
+
+    await substation.related('works').query().delete()
+    await substation.delete()
+
+    return response.status(204)
+  }
 }
