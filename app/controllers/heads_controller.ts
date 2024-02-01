22feaa93b4d2d2805e42faed960b931ef51d2fca
BolotnikovMS <bolotnikovms@yandex.ru>
2024-02-01 15:35:47 +0500

    feat: Контроллеры

--- app/controllers/heads_controller.ts
+++ app/controllers/heads_controller.ts
@@ -1,38 +1,47 @@
+import HeadController from '#models/head_controller'
+import { HeadControllersService } from '#services/head_controller_service'
 import type { HttpContext } from '@adonisjs/core/http'
+import { headControllerValidator } from '#validators/head_controller'
 
 export default class HeadsController {
   /**
    * Display a list of resource
    */
-  async index({}: HttpContext) {}
+  async index({ request, response }: HttpContext) {
+    const headControllers = await HeadControllersService.getHeadControllers(request)
 
-  /**
-   * Display form to create a new record
-   */
-  async create({}: HttpContext) {}
+    return response.status(200).json(headControllers)
+  }
 
   /**
    * Handle form submission for the create action
    */
-  async store({ request }: HttpContext) {}
+  async store({ request, response }: HttpContext) {
+    const validatedData = await request.validateUsing(headControllerValidator)
+    const newHeadController = await HeadController.create({ userId: 1, ...validatedData })
 
-  /**
-   * Show individual record
-   */
-  async show({ params }: HttpContext) {}
-
-  /**
-   * Edit individual record
-   */
-  async edit({ params }: HttpContext) {}
+    return response.status(201).json(newHeadController)
+  }
 
   /**
    * Handle form submission for the edit action
    */
-  async update({ params, request }: HttpContext) {}
+  async update({ params, request, response }: HttpContext) {
+    const headController = await HeadController.findOrFail(params.id)
+    const validatedData = await request.validateUsing(headControllerValidator)
+    const updHeadController = await headController.merge(validatedData).save()
+
+    return response.status(200).json(updHeadController)
+  }
 
   /**
    * Delete record
    */
-  async destroy({ params }: HttpContext) {}
+  async destroy({ params, response }: HttpContext) {
+    const headController = await HeadController.findOrFail(params.id)
+
+    await headController.delete()
+
+    return response.status(204)
+  }
 }
--- app/controllers/types_kps_controller.ts
+++ app/controllers/types_kps_controller.ts
@@ -1,38 +1,47 @@
+import TypeKp from '#models/type_kp'
+import TypeKpService from '#services/type_kp_service'
+import { typeKpValidator } from '#validators/type_kp'
 import type { HttpContext } from '@adonisjs/core/http'
 
 export default class TypesKpsController {
   /**
    * Display a list of resource
    */
-  async index({}: HttpContext) {}
+  async index({ request, response }: HttpContext) {
+    const typesKps = await TypeKpService.getTypesKps(request)
 
-  /**
-   * Display form to create a new record
-   */
-  async create({}: HttpContext) {}
+    return response.status(200).json(typesKps)
+  }
 
   /**
    * Handle form submission for the create action
    */
-  async store({ request }: HttpContext) {}
+  async store({ request, response }: HttpContext) {
+    const validatedData = await request.validateUsing(typeKpValidator)
+    const typeKp = await TypeKp.create({ userId: 1, ...validatedData })
 
-  /**
-   * Show individual record
-   */
-  async show({ params }: HttpContext) {}
-
-  /**
-   * Edit individual record
-   */
-  async edit({ params }: HttpContext) {}
+    return response.status(201).json(typeKp)
+  }
 
   /**
    * Handle form submission for the edit action
    */
-  async update({ params, request }: HttpContext) {}
+  async update({ params, request, response }: HttpContext) {
+    const typeKp = await TypeKp.findOrFail(params.id)
+    const validatedData = await request.validateUsing(typeKpValidator)
+    const updTypeKp = await typeKp.merge(validatedData).save()
+
+    return response.status(200).json(updTypeKp)
+  }
 
   /**
    * Delete record
    */
-  async destroy({ params }: HttpContext) {}
+  async destroy({ params, response }: HttpContext) {
+    const typeKp = await TypeKp.findOrFail(params.id)
+
+    await typeKp.delete()
+
+    return response.status(204)
+  }
 }
