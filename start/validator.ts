2bcbbd33a99d2113fec619c5364a748d9075a0d0
BolotnikovMS <bolotnikovms@yandex.ru>
2024-02-01 21:36:21 +0500

    feat: Добавлено сообщение для ip

--- start/validator.ts
+++ start/validator.ts
@@ -5,6 +5,7 @@ vine.messagesProvider = new SimpleMessagesProvider({
   'required': 'Поле является обязательным.',
   'minLength': 'Минимальная длина {{ min }} символа.',
   'maxLength': 'Максимальная длина {{ max }} символов.',
+  'ipAddress': 'Поле с ip адресом должно быть формата xxx.xxx.xxx.xxx.',
 
   // Error message for the username field
   'username.required': 'Please choose a username for your account',
