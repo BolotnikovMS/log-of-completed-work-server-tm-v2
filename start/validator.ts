e733a3c9baca31b1852238e213f9068a337934af
BolotnikovMS <bolotnikovms@yandex.ru>
2024-02-11 20:22:36 +0500

    feat: Добавлено кастомное сообщение

--- start/validator.ts
+++ start/validator.ts
@@ -6,6 +6,7 @@ vine.messagesProvider = new SimpleMessagesProvider({
   'minLength': 'Минимальная длина {{ min }} символа.',
   'maxLength': 'Максимальная длина {{ max }} символов.',
   'ipAddress': 'Поле с ip адресом должно быть формата xxx.xxx.xxx.xxx.',
+  'date': 'Дата и время должны иметь формат: {{ format }}.',
 
   // Error message for the username field
   'username.required': 'Please choose a username for your account',
