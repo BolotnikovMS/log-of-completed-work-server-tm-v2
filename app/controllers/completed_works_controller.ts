932f4b22fd5993bc97b0ba57b75cf2b4cf65b7ed
BolotnikovMS <bolotnikovms@yandex.ru>
2024-02-11 20:24:37 +0500

    feat: Добавлены методы

--- app/controllers/completed_works_controller.ts
+++ app/controllers/completed_works_controller.ts
@@ -1,20 +1,27 @@
+import CompletedWork from '#models/completed_work'
+import CompletedWorkService from '#services/completed_wokr_service'
+import { completedWorkValidator } from '#validators/completed_work'
 import type { HttpContext } from '@adonisjs/core/http'
 
 export default class CompletedWorksController {
   /**
    * Display a list of resource
    */
-  async index({}: HttpContext) {}
+  async index({ request, response }: HttpContext) {
+    const works = await CompletedWorkService.getCompletedWorks(request)
 
-  /**
-   * Display form to create a new record
-   */
-  async create({}: HttpContext) {}
+    return response.status(200).json(works)
+  }
 
   /**
    * Handle form submission for the create action
    */
-  async store({ request }: HttpContext) {}
+  async store({ request, response }: HttpContext) {
+    const validatedData = request.validateUsing(completedWorkValidator)
+    const completedWork = await CompletedWork.create({ userId: 1, ...validatedData })
+
+    return response.status(201).json(completedWork)
+  }
 
   /**
    * Show individual record
@@ -22,17 +29,26 @@ export default class CompletedWorksController {
   async show({ params }: HttpContext) {}
 
   /**
-   * Edit individual record
-   */
-  async edit({ params }: HttpContext) {}
-
-  /**
    * Handle form submission for the edit action
    */
-  async update({ params, request }: HttpContext) {}
+  async update({ params, request, response }: HttpContext) {
+    const completedWork = await CompletedWork.findOrFail(params.id)
+    const validatedData = await request.validateUsing(completedWorkValidator)
+    const updCompletedWork = await completedWork
+      .merge({ workProducerId: 1, ...validatedData })
+      .save()
+
+    return response.status(200).json(updCompletedWork)
+  }
 
   /**
    * Delete record
    */
-  async destroy({ params }: HttpContext) {}
+  async destroy({ params, response }: HttpContext) {
+    const completedWork = await CompletedWork.findOrFail(params.id)
+
+    await completedWork.delete()
+
+    return response.status(204)
+  }
 }
