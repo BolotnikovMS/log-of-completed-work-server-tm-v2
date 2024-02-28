6aac1ff20edb8057868e059d10eaf5481784c5c8
BolotnikovMS <bolotnikovms@yandex.ru>
2024-02-28 18:44:46 +0500

    feat: Кастомный метод для валидации

---
+++ app/rules/unique.ts
@@ -0,0 +1,41 @@
+import db from '@adonisjs/lucid/services/db'
+import vine from '@vinejs/vine'
+import { FieldContext } from '@vinejs/vine/types'
+
+type TOptions = {
+  table: string
+  column: string
+  caseInsensitive?: boolean
+}
+
+/**
+ * Description
+ * @param {unknown} value. Значение для проверки на уникальность в таблице бд.
+ * @param {TOptions} options. Набор параметров. Название таблицы в бд, имя столбца, чувствительность к регистру.
+ * @param {FieldContext} field. Название поля.
+ * @returns {any}
+ */
+
+async function unique(value: unknown, options: TOptions, field: FieldContext) {
+  /**
+   * We do not want to deal with non-string
+   * values. The "string" rule will handle the
+   * the validation.
+   */
+  if (typeof value !== 'string') {
+    return
+  }
+
+  const { table, column, caseInsensitive } = options
+  const row = await db
+    .from(table)
+    .if(!!caseInsensitive === true, (query) => query.whereLike(column, value))
+    .if(!!caseInsensitive === false, (query) => query.where(column, value))
+    .first()
+
+  if (row) {
+    field.report('The {{ field }} field is not unique', 'unique', field)
+  }
+}
+
+export const uniqueRule = vine.createRule(unique)
--- start/validator.ts
+++ start/validator.ts
@@ -7,6 +7,7 @@ vine.messagesProvider = new SimpleMessagesProvider({
   'maxLength': 'Максимальная длина {{ max }} символов.',
   'ipAddress': 'Поле с ip адресом должно быть формата xxx.xxx.xxx.xxx.',
   'date': 'Дата и время должны иметь формат: {{ format }}.',
+  'unique': 'Поле {{ field }} должно быть уникальным.',
 
   // Error message for the username field
   'username.required': 'Please choose a username for your account',
