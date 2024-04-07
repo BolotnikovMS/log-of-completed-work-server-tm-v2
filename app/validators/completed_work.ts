646c7aba56734d81ec7ba5147e403d8a6bd3f229
BolotnikovMS <bolotnikovms@yandex.ru>
2024-04-07 19:40:51 +0500

    fix: Добавлен формат даты

--- app/validators/completed_work.ts
+++ app/validators/completed_work.ts
@@ -5,6 +5,9 @@ export const completedWorkValidator = vine.compile(
     substationId: vine.number(),
     description: vine.string().trim().minLength(2).maxLength(1000).escape(),
     note: vine.string().trim().minLength(3).maxLength(700).optional(),
-    dateCompletion: vine.date(),
+    workProducerId: vine.number(),
+    dateCompletion: vine.date({
+      formats: 'DD.MM.YYYY',
+    }),
   })
 )
