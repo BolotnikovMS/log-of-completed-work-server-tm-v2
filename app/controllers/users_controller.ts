a66c427f8a9788c085be34f3b4b1a4ca1f0dbcae
BolotnikovMS <bolotnikovms@yandex.ru>
2024-04-07 19:40:14 +0500

    feat: User controller

---
+++ app/controllers/users_controller.ts
@@ -0,0 +1,43 @@
+import User from '#models/user'
+import type { HttpContext } from '@adonisjs/core/http'
+
+export default class UsersController {
+  /**
+   * Display a list of resource
+   */
+  async index({ request, response }: HttpContext) {
+    const users = await User.query()
+
+    return response.status(200).json(users)
+  }
+
+  /**
+   * Display form to create a new record
+   */
+  async create({}: HttpContext) {}
+
+  /**
+   * Handle form submission for the create action
+   */
+  async store({ request }: HttpContext) {}
+
+  /**
+   * Show individual record
+   */
+  async show({ params }: HttpContext) {}
+
+  /**
+   * Edit individual record
+   */
+  async edit({ params }: HttpContext) {}
+
+  /**
+   * Handle form submission for the edit action
+   */
+  async update({ params, request }: HttpContext) {}
+
+  /**
+   * Delete record
+   */
+  async destroy({ params }: HttpContext) {}
+}
