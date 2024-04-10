80c4a52dc2b857f54aaf694e6cf6a3eb160d09e5
BolotnikovMS <bolotnikovms@yandex.ru>
2024-04-10 19:11:06 +0500

    feat: Сервис для работы с пользователями
    Добавлен метод для получения всех пользователей

---
+++ app/services/user_service.ts
@@ -0,0 +1,14 @@
+import { IQueryParams } from '#interfaces/query_params'
+import User from '#models/user'
+import { Request } from '@adonisjs/core/http'
+
+export default class UserService {
+  static async getUsers(req: Request): Promise<User[]> {
+    const { active, page, limit } = req.qs() as IQueryParams
+    const users = await User.query()
+      .if(active, (query) => query.where('active', active))
+      .paginate(page, limit)
+
+    return users
+  }
+}
