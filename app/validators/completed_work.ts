fd5e0805ec6cb9d52e6dcd1fd6c9843f0e90271f
BolotnikovMS <bolotnikovms@yandex.ru>
2024-05-01 18:05:33 +0500

    feat: Переделан в строку

--- app/validators/completed_work.ts
+++ app/validators/completed_work.ts
@@ -6,8 +6,9 @@ export const completedWorkValidator = vine.compile(
     description: vine.string().trim().minLength(2).maxLength(1000).escape(),
     note: vine.string().trim().minLength(3).maxLength(700).optional(),
     workProducerId: vine.number(),
-    dateCompletion: vine.date({
-      formats: 'DD.MM.YYYY',
-    }),
+    // dateCompletion: vine.date({
+    //   formats: 'DD.MM.YYYY',
+    // }),
+    dateCompletion: vine.string(),
   })
 )
