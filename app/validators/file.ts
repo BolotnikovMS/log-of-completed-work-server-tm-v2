a84f25ca872d90644405913d9c9b5d3eb2bea07e
BolotnikovMS <bolotnikovms@yandex.ru>
2024-05-11 13:57:42 +0500

    feat: Схема валидации файла

---
+++ app/validators/file.ts
@@ -0,0 +1,9 @@
+import vine from '@vinejs/vine'
+
+export const fileValidator = vine.compile(
+  vine.object({
+    substationId: vine.number(),
+    file: vine.array(vine.file()).notEmpty(),
+    typeFile: vine.string().in(['photo_ps', 'backup']),
+  })
+)
