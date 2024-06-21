372a460183c273ac5ac82da6697a1ca263efe14c
BolotnikovMS <bolotnikovms@yandex.ru>
2024-06-21 16:15:29 +0500

    feat: Добавлен метод для смены пароля

--- app/services/user_service.ts
+++ app/services/user_service.ts
@@ -1,5 +1,6 @@
 import { IQueryParams } from '#interfaces/query_params'
 import User from '#models/user'
+import { changePasswordValidator } from '#validators/change_password'
 import { Request } from '@adonisjs/core/http'
 
 export default class UserService {
@@ -11,5 +12,12 @@ export default class UserService {
       .paginate(page, limit)
 
     return users
+  }
+  static async getClearUsers(req: Request): Promise<User[]> { }
+  static async changePassword(req: Request, userId: number): Promise<User> {
+    const user = await User.findOrFail(userId)
+    const validatedData = await req.validateUsing(changePasswordValidator)
+
+    return await user.merge(validatedData).save()
   }
 }
