f19aac8379032476249b843be1ac3445baecc900
BolotnikovMS <bolotnikovms@yandex.ru>
2024-08-13 13:57:42 +0500

    feat: Добавлен маршрут для скачивания excel

--- start/routes/api/v1/completed_works.ts
+++ start/routes/api/v1/completed_works.ts
@@ -8,6 +8,7 @@ export const completedWorksRoutes = router
     router.post('/', [CompletedWorksController, 'store'])
     router.patch('/:id', [CompletedWorksController, 'update'])
     router.delete('/:id', [CompletedWorksController, 'destroy'])
+    router.get('/download-excel', [CompletedWorksController, 'downloadExcel'])
   })
   .prefix('/completed-works')
   .use([middleware.auth()])
