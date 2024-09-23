5e57e5abfd0d3347fb325b5a20cd0eda3fa9e852
BolotnikovMS <bolotnikovms@yandex.ru>
2024-09-23 19:06:25 +0500

    feat: Метод для получения статистики
    Метод возвращает количество выполненных работ по годам

--- app/services/dashboard_service.ts
+++ app/services/dashboard_service.ts
@@ -1,4 +1,6 @@
+import CompletedWork from "#models/completed_work"
 import TypeKp from "#models/type_kp"
+import db from "@adonisjs/lucid/services/db"
 
 export default class DashboardService {
   static async getSubstationsTypeKp() {
@@ -8,5 +10,19 @@ export default class DashboardService {
     }))
 
     return typesKpSerialize
+  }
+  static async getCompletedWorksYear() {
+    const completedWorks = await CompletedWork.query()
+      .select(db.raw("strftime('%Y', date_completion) as year"))
+      .count('* as total')
+      .groupBy('year')
+      .orderBy('year', 'asc')
+
+    const result = completedWorks.map(work => ({
+      year: work.$extras.year,
+      workCount: work.$extras.total
+    }))
+
+    return result
   }
 }
