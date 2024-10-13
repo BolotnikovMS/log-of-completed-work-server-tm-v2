7168976578bf975669f8c702228872684c5cbefc
BolotnikovMS <bolotnikovms@yandex.ru>
2024-10-13 18:20:46 +0500

    feat: Добавлены поля

--- app/helpers/transform_channel_data.ts
+++ app/helpers/transform_channel_data.ts
@@ -5,15 +5,19 @@ export interface ITransformedChannelsData {
   fullNameSubstation: string
   channelCategory: string
   channelType: string
+  channelEquipment: string | null
+  gsmOperator: string | null
   ipAddress: string | null
   note: string | null
 }
 
 export const transformDataChannels = (data: ModelObject[]): ITransformedChannelsData[] => {
   return data.map(channel => ({
-    fullNameSubstation: channel?.substation?.fullNameSubstation ?? 'Нет',
+    fullNameSubstation: channel?.substation.fullNameSubstation ?? 'Нет',
     channelCategory: channel?.channel_category.name ?? 'Нет',
     channelType: channel?.channel_type.name ?? 'Нет',
+    channelEquipment: channel?.channel_equipment?.name ?? 'Нет',
+    gsmOperator: channel?.gsm_operator?.name ?? 'Нет',
     ipAddress: channel.ipAddress ?? 'Нет',
     note: channel.note ?? 'Нет'
   }))
