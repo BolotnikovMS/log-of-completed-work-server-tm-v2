b4ebc09a3f0962c8a3bda45de8f4e7b78cb2d5d3
BolotnikovMS <bolotnikovms@yandex.ru>
2024-10-13 18:24:43 +0500

    feat: Добавлены поля в схему валидации

--- app/validators/channel.ts
+++ app/validators/channel.ts
@@ -1,11 +1,13 @@
+import { ipOptional, numberCheck, numberOptional, text700Optional } from '#validators/fields_check'
 import vine from '@vinejs/vine'
-import { ipOptional, numberCheck, text700Optional } from '#validators/fields_check'
 
 export const channelValidator = vine.compile(
   vine.object({
     substationId: numberCheck,
     channelCategoryId: numberCheck,
     channelTypeId: numberCheck,
+    channelEquipmentId: numberOptional,
+    gsmId: numberOptional,
     ipAddress: ipOptional,
     note: text700Optional,
   })
