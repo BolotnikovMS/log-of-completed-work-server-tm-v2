f8cdd09e3268e226c8cf31cba53ad8126b9b3819
BolotnikovMS <bolotnikovms@yandex.ru>
2024-10-15 20:14:39 +0500

    feat: Добавлена миграция с добавлением колонки

---
+++ database/migrations/1729004551078_create_completed_work_add_type_works_table.ts
@@ -0,0 +1,24 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'completed_works'
+
+  async up() {
+    this.schema.alterTable(this.tableName, (table) => {
+      table
+        .integer('type_work_id', 11)
+        .notNullable()
+        .index()
+        .unsigned()
+        .references('id')
+        .inTable('types_works')
+        .defaultTo(1)
+    })
+  }
+
+  async down() {
+    this.schema.alterTable(this.tableName, (table) => {
+      table.dropColumn('type_work_id')
+    })
+  }
+}
