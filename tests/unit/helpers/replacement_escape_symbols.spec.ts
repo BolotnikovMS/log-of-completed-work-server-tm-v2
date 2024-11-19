8645be59d50110e1abd4eb0a810cf6423ebf7c9b
Mikhail <bolotnikovms@yandex.ru>
2024-11-19 20:20:11 +0500

    test: Тест функций для замены спец символов

---
+++ tests/unit/helpers/replacement_escape_symbols.spec.ts
@@ -0,0 +1,18 @@
+import { replacementEscapeSymbols } from '#helpers/replacement_escape_symbols'
+import { test } from '@japa/runner'
+
+test.group('Helpers replacement escape symbols', () => {
+  test('Проверка на замену экранированных символов в строке')
+    .with([
+      { text: '&quot;Test&quot;', result: '"Test"' },
+      { text: 'And&amp;', result: 'And&' },
+      { text: '2&lt;1', result: '2<1' },
+      { text: 'TestClear', result: 'TestClear' },
+      { text: '', result: '' },
+      { text: null, result: null },
+    ])
+    .run(({ assert }, item) => {
+      // console.log(`Значение ${item.text} = ${item.result}`)
+      assert.equal(replacementEscapeSymbols(item.text), item.result, `Value: ${item.text}`)
+    })
+})
