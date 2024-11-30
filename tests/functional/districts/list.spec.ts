3d8bba7e5472cf8e410875034a01e72bb121e8f7
Mikhail <bolotnikovms@yandex.ru>
2024-11-30 22:06:13 +0500

    feat: Добавлены тесты
    1. Тест для проверки добавления данных с пограничными значениями для валидации.
    2. Тест для проверки добавления без прав у пользователя.

--- tests/functional/districts/list.spec.ts
+++ tests/functional/districts/list.spec.ts
@@ -1,8 +1,9 @@
+import { accessErrorMessages } from '#helpers/access_error_messages'
 import User from '#models/user'
 import testUtils from '@adonisjs/core/services/test_utils'
 import { test } from '@japa/runner'
 
-test.group('Districts list test', (group) => {
+test.group('Тесты для проверки функционала "Районов"', (group) => {
   group.each.setup(() => testUtils.db().withGlobalTransaction())
 
   test('Получить все записи районов с корректными токеном', async ({ client, assert }) => {
@@ -22,7 +23,7 @@ test.group('Districts list test', (group) => {
     resp.assertBodyContains({ errors: [{ message: 'Unauthorized access' }] })
   })
 
-  test('Добавить новый район с корректным токеном и правами', async ({ client, assert }) => {
+  test('Добавить новый район с корректным токеном, данными и правами', async ({ client, assert }) => {
     const user = await User.findOrFail(1)
     const testDistrict = {
       name: 'Test distric',
@@ -38,5 +39,43 @@ test.group('Districts list test', (group) => {
     resp.assertStatus(201)
     resp.assertHeader('content-type', 'application/json; charset=utf-8')
     assert.isNumber(resp.body().id)
+  })
+
+  test('Добавить новый район с корректным токеном, правами и пограничными значениями', async ({ client }) => {
+    const user = await User.findOrFail(1)
+    const testData = {
+      name: 'Te',
+      shortName: 'te'
+    }
+    const resp = await client
+      .post('/api/v1.0/districts')
+      .json(testData)
+      .withGuard('api')
+      .loginAs(user)
+
+    resp.dumpBody()
+    resp.assertStatus(201)
+    resp.assertHeader('content-type', 'application/json; charset=utf-8')
+    resp.assertBodyContains(testData)
+  })
+
+  // Тест с не корректнвми данными
+
+
+  test('Добавить новый район с корректным токеном, но без прав', async ({ client }) => {
+    const user = await User.findOrFail(2)
+    const testDistrict = {
+      name: 'Test distric',
+      shortName: 'Test'
+    }
+    const resp = await client
+      .post('/api/v1.0/districts')
+      .json(testDistrict)
+      .withGuard('api')
+      .loginAs(user)
+
+    // resp.dumpBody()
+    resp.assertStatus(403)
+    resp.assertBodyContains({ message: accessErrorMessages.create })
   })
 })
