5d85a0dbd0ca64ed6d15813ba4470031d7e95d01
Mikhail <bolotnikovms@yandex.ru>
2025-01-15 21:31:50 +0500

    feat: Исключены маршруты

--- config/swagger.ts
+++ config/swagger.ts
@@ -10,21 +10,19 @@ export default {
   description: "Test info", // use info instead
   tagIndex: 3,
   info: {
-    title: "title",
+    title: "API info",
     version: "1.0.0",
     description: "",
   },
   snakeCase: true,
   debug: false, // set to true, to get some useful debug output
-  ignore: ["/swagger", "/docs"],
+  ignore: ["/swagger", "/docs", "/api/v1.0/login", "/api/v1.0/logout", "/api/v1.0/change-password", "/api/v1.0/users/create-account", "/api/v1.0/users/reset-password/:id", "/api/v1.0/users/block-user-account/:id", "/api/v1.0/users/change-role/:id", "/api/v1.0/users/roles", "/api/v1.0/files/*"],
   preferredPutPatch: "PUTCH", // if PUT/PATCH are provided for the same route, prefer PUT
   common: {
     parameters: {}, // OpenAPI conform parameters that are commonly used
     headers: {}, // OpenAPI conform headers that are commonly used
   },
-  securitySchemes: {
-    
-  }, // optional
+  securitySchemes: {}, // optional
   authMiddlewares: ["auth", "auth:api"], // optional
   defaultSecurityScheme: "BearerAuth", // optional
   persistAuthorization: true, // persist authorization between reloads on the swagger page
