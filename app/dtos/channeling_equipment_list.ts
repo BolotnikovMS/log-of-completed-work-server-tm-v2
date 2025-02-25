382df0747e58e20e265d129b1144eaa471a55613
Mikhail <bolotnikovms@yandex.ru>
2025-02-25 20:01:24 +0500

    feat: Dto списка оборудования

---
+++ app/dtos/channeling_equipment_list.ts
@@ -0,0 +1,16 @@
+import ChannelingEquipment from '#models/channeling_equipment'
+import { BaseModelDto } from '@adocasts.com/dto/base'
+
+export default class ChannelingEquipmentListDto extends BaseModelDto {
+  declare id: number
+  declare name: string
+
+  constructor(channelingEquipment?: ChannelingEquipment) {
+    super()
+
+    if (!channelingEquipment) return
+
+    this.id = channelingEquipment.id
+    this.name = channelingEquipment.name
+  }
+}
