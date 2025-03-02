0a53685d1b2c7da2c75a46aef9880a0a44a8aaaf
Mikhail <bolotnikovms@yandex.ru>
2025-03-02 19:40:26 +0500

    feat: dto файлов объекта

---
+++ app/dtos/substation_file_list.ts
@@ -0,0 +1,24 @@
+import File from '#models/file'
+import { BaseModelDto } from '@adocasts.com/dto/base'
+
+export default class SubstationFileListDto extends BaseModelDto {
+  declare id: number
+  declare filePath: string
+  declare clientName: string
+  declare size: number
+  declare createdAt: string
+  declare author: string | null
+
+  constructor(file?: File) {
+    super()
+
+    if (!file) return
+
+    this.id = file.id
+    this.filePath = file.filePath
+    this.clientName = file.clientName
+    this.size = file.size
+    this.createdAt = file.createdAt.toString()!
+    this.author = file.author.shortName
+  }
+}
