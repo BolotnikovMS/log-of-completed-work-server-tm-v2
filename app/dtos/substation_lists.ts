52ba5b48374d96a821ebe5e13bea9744eb7d0fad
Mikhail <bolotnikovms@yandex.ru>
2025-03-02 15:52:58 +0500

    refactor: dto списка объектов

--- app/dtos/substation_lists.ts
+++ app/dtos/substation_lists.ts
@@ -3,12 +3,6 @@ import { BaseModelDto } from '@adocasts.com/dto/base'
 
 export default class SubstationListDto extends BaseModelDto {
   declare id: number
-  declare active: boolean
-  declare districtId: number
-  declare voltageClassesId: number
-  declare typeKpId: number
-  declare headControllerId: number
-  declare objectTypeId: number
   declare object_type: string | null
   declare name: string
   declare rdu: boolean
@@ -20,13 +14,6 @@ export default class SubstationListDto extends BaseModelDto {
     if (!substation) return
 
     this.id = substation.id
-    this.active = substation.active
-    this.districtId = substation.districtId
-    this.voltageClassesId = substation.voltageClassesId
-    this.typeKpId = substation.typeKpId
-    this.headControllerId = substation.headControllerId
-    this.objectTypeId = substation.objectTypeId
-    this.name = substation.name
     this.rdu = substation.rdu
     this.fullNameSubstation = substation.fullNameSubstation ?? substation.name
     this.object_type = substation.object_type?.shortName
