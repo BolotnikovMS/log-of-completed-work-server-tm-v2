f0de16b943b1a5bd9300be131900185c3497c42c
Mikhail <bolotnikovms@yandex.ru>
2025-03-04 20:34:41 +0500

    feat: Добавлены dto

---
+++ app/dtos/completed_work_info.ts
@@ -0,0 +1,28 @@
+import CompletedWork from '#models/completed_work'
+import { BaseModelDto } from '@adocasts.com/dto/base'
+
+export default class CompletedWorkInfoDto extends BaseModelDto {
+  declare id: number
+  declare substation: string
+  declare dateCompletion: string
+  declare type_work: string
+  declare work_producer: string
+  declare author: string
+  declare description: string
+  declare note: string | null
+
+  constructor(completedWork?: CompletedWork) {
+    super()
+
+    if (!completedWork) return
+
+    this.id = completedWork.id
+    this.substation = completedWork.substation.fullNameSubstation
+    this.dateCompletion = completedWork.dateCompletion.toString()
+    this.type_work = completedWork.type_work.name
+    this.work_producer = completedWork.work_producer.shortName
+    this.author = completedWork.author.shortName
+    this.description = completedWork.description
+    this.note = completedWork.note
+  }
+}
---
+++ app/dtos/completed_work_list.ts
@@ -0,0 +1,28 @@
+import CompletedWork from '#models/completed_work'
+import { BaseModelDto } from '@adocasts.com/dto/base'
+
+export default class CompletedWorkListDto extends BaseModelDto {
+  declare id: number
+  declare substationId: number
+  declare description: string
+  declare dateCompletion: string
+  declare inControl: boolean
+  declare work_producer: string
+  declare type_work: string
+  declare substation: string
+
+  constructor(completedWork?: CompletedWork) {
+    super()
+
+    if (!completedWork) return
+
+    this.id = completedWork.id
+    this.substationId = completedWork.substationId
+    this.description = completedWork.description
+    this.dateCompletion = completedWork.dateCompletion.toString()
+    this.inControl = completedWork.inControl
+    this.work_producer = completedWork.work_producer.shortName
+    this.type_work = completedWork.type_work.name
+    this.substation = completedWork.substation.fullNameSubstation
+  }
+}
