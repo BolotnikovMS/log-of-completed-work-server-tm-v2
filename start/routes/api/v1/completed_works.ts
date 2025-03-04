9b17e2a69ccf2fe52bf8a02be2758c19fdfa2c9c
Mikhail <bolotnikovms@yandex.ru>
2025-03-04 20:36:09 +0500

    feat: Добавлены маршруты

--- start/routes/api/v1/completed_works.ts
+++ start/routes/api/v1/completed_works.ts
@@ -5,6 +5,8 @@ import router from "@adonisjs/core/services/router"
 export const completedWorksRoutes = router
   .group(() => {
     router.get('/', [CompletedWorksController, 'index'])
+    router.get('/:id', [CompletedWorksController, 'getCompletedWork'])
+    router.get('/:id/info', [CompletedWorksController, 'getCompletedWorkInfo'])
     router.post('/', [CompletedWorksController, 'store'])
     router.patch('/:id', [CompletedWorksController, 'update'])
     router.delete('/:id', [CompletedWorksController, 'destroy'])
