9fd3d86ce47a75f9fd9813662e3f62277ad2b2fc
Mikhail <bolotnikovms@yandex.ru>
2025-03-04 20:35:13 +0500

    refactor: Поправлен dto

--- app/dtos/completed_work.ts
+++ app/dtos/completed_work.ts
@@ -3,7 +3,6 @@ import { BaseModelDto } from '@adocasts.com/dto/base'
 
 export default class CompletedWorkDto extends BaseModelDto {
   declare id: number
-  declare userId: number
   declare substationId: number
   declare workProducerId: number
   declare typeWorkId: number
@@ -11,10 +10,6 @@ export default class CompletedWorkDto extends BaseModelDto {
   declare note: string | null
   declare dateCompletion: string
   declare inControl: boolean
-  declare work_producer: string | null
-  declare author: string | null
-  declare type_work: string | null
-  declare substation: string | null
 
   constructor(completedWork?: CompletedWork) {
     super()
@@ -22,17 +17,12 @@ export default class CompletedWorkDto extends BaseModelDto {
     if (!completedWork) return
 
     this.id = completedWork.id
-    this.userId = completedWork.userId
     this.substationId = completedWork.substationId
     this.workProducerId = completedWork.workProducerId
     this.typeWorkId = completedWork.typeWorkId
     this.description = completedWork.description
     this.note = completedWork.note
-    this.dateCompletion = completedWork.dateCompletion.toString()!
+    this.dateCompletion = completedWork.dateCompletion.toString()
     this.inControl = completedWork.inControl
-    this.work_producer = completedWork.work_producer?.shortName
-    this.author = completedWork.author?.shortName
-    this.type_work = completedWork.type_work?.name
-    this.substation = completedWork.substation?.fullNameSubstation
   }
 }
