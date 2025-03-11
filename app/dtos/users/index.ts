4029f3daf118a1b898c53fc2715f5bd6608aab0a
Mikhail <bolotnikovms@yandex.ru>
2025-03-11 14:55:38 +0500

    feat: Добавлены dto

---
+++ app/dtos/users/index.ts
@@ -0,0 +1,5 @@
+import UserBaseDto from './user_base.js'
+import UserFullDto from './user_full.js'
+import UserShortDto from './user_short.js'
+
+export { UserBaseDto, UserFullDto, UserShortDto }
---
+++ app/dtos/users/user_base.ts
@@ -0,0 +1,26 @@
+import User from '#models/user'
+import { BaseModelDto } from '@adocasts.com/dto/base'
+
+export default class UserBaseDto extends BaseModelDto {
+  declare id: number
+  declare active: boolean
+  declare username: string
+  declare role: string
+  declare email: string
+  declare shortName: string
+  declare fullName: string
+
+  constructor(user?: User) {
+    super()
+
+    if (!user) return
+
+    this.id = user.id
+    this.active = user.active
+    this.username = user.username
+    this.role = user.role.name
+    this.email = user.email
+    this.shortName = user.shortName
+    this.fullName = user.fullName
+  }
+}
