275262017247abfc27dcd730335161416fead662
Mikhail <bolotnikovms@yandex.ru>
2025-04-06 15:20:43 +0500

    fix: Добавлена проверка свойств

--- app/dtos/channels/channel_info.ts
+++ app/dtos/channels/channel_info.ts
@@ -1,13 +1,12 @@
 import { BaseModelDto } from '@adocasts.com/dto/base'
 import Channel from '#models/channel'
-import { ChannelCategoryDto } from '#dtos/channel_categories/index'
 
 export default class ChannelInfoDto extends BaseModelDto {
   declare id: number
   declare ipAddress: string | null
   declare note: string | null
   declare substation: string | null
-  declare channel_category: ChannelCategoryDto | null
+  declare channel_category: string | null
   declare channel_type: string | null
   declare channel_equipment: string | null
 
@@ -17,11 +16,11 @@ export default class ChannelInfoDto extends BaseModelDto {
     if (!channel) return
 
     this.id = channel.id
-    this.ipAddress = channel.ipAddress
-    this.note = channel.note
-    this.substation = channel.substation.fullNameSubstation
-    this.channel_category = channel.channel_category && new ChannelCategoryDto(channel.channel_category)
-    this.channel_type = channel.channel_type.name
-    this.channel_equipment = channel.channel_equipment.name
+    this.ipAddress = channel?.ipAddress
+    this.note = channel?.note
+    this.substation = channel.substation?.fullNameSubstation
+    this.channel_category = channel.channel_category?.name
+    this.channel_type = channel.channel_type?.name
+    this.channel_equipment = channel.channel_equipment?.name
   }
 }
