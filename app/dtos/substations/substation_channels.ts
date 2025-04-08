63894aa3620e654855ddae75dcefd16ca3678303
Mikhail <bolotnikovms@yandex.ru>
2025-04-08 14:47:54 +0500

    fix: Поправлены поля

--- app/dtos/substations/substation_channels.ts
+++ app/dtos/substations/substation_channels.ts
@@ -4,11 +4,8 @@ import { BaseModelDto } from '@adocasts.com/dto/base'
 export default class SubstationChannelsDto extends BaseModelDto {
   declare id: number
   declare ipAddress: string | null
-  declare note: string | null
   declare channel_category_short: string | null
   declare channel_type: string | null
-  declare channel_equipment: string | null
-  declare gsm: string | null
 
   constructor(channel?: Channel) {
     super()
@@ -17,10 +14,7 @@ export default class SubstationChannelsDto extends BaseModelDto {
 
     this.id = channel.id
     this.ipAddress = channel.ipAddress
-    this.note = channel.note
     this.channel_category_short = channel.channel_category?.shortName
     this.channel_type = channel.channel_type?.name
-    this.channel_equipment = channel.channel_equipment?.name
-    this.gsm = channel.gsm_operator?.name
   }
 }
