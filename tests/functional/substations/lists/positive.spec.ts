8c681efbd24a10fb50ef34ff3ed93f955598c13e
Mikhail <bolotnikovms@yandex.ru>
2025-07-06 19:35:23 +0500

    feat(positive): Набор позитивных тестов

---
+++ tests/functional/substations/lists/positive.spec.ts
@@ -0,0 +1,38 @@
+import { RolesEnum } from '#enums/roles'
+import User from '#models/user'
+import testUtils from '@adonisjs/core/services/test_utils'
+import { test } from '@japa/runner'
+
+test.group('✅ Позитивные тесты. Тесты для проверки функционала "Обновления ключа связт с ЖД".', group => {
+  const id = 4
+  const urlApi = `/api/v1.0/substations/${id}/add-key-defects`
+  let admin: User
+  const correctData = [
+    { keyDefectSubstation: 1, resultTest: 1 },
+    { keyDefectSubstation: null, resultTest: null },
+    { keyDefectSubstation: '', resultTest: null },
+  ]
+
+  group.setup(async () => {
+    admin = await User.findOrFail(1)
+  })
+  group.each.setup(() => testUtils.db().withGlobalTransaction())
+
+  test('Обновление ключа валидными значениями(Установка/сброс ключа).')
+    .with(correctData)
+    .run(async ({ client, assert }, item) => {
+      const resp = await client
+        .patch(urlApi)
+        .json(item)
+        .withGuard('api')
+        .loginAs(admin)
+
+      resp.assertStatus(200)
+      resp.assertHeader('content-type', 'application/json; charset=utf-8')
+      assert.equal(RolesEnum.ADMIN, admin.roleId)
+      assert.isObject(resp.body())
+      assert.properties(resp.body(), ['id', 'keyDefectSubstation'])
+      assert.equal(id, resp.body().id)
+      assert.equal(item.resultTest, resp.body().keyDefectSubstation)
+    })
+})
