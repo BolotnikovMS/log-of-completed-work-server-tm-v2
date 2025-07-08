48af986984de80974e13c5cf5940afe8282ddecc
Mikhail <bolotnikovms@yandex.ru>
2025-07-08 15:53:59 +0500

    fix: Поправлены сообщения

--- start/validator.ts
+++ start/validator.ts
@@ -5,11 +5,13 @@ vine.messagesProvider = new SimpleMessagesProvider({
   'required': 'Поле является обязательным.',
   'minLength': 'Минимальная длина {{ min }} символа.',
   'maxLength': 'Максимальная длина {{ max }} символов.',
-  'ipAddress': 'Поле с ip адресом должно быть формата xxx.xxx.xxx.xxx.',
+  'ipAddress': 'Введенный ip адрес должен быть в формате xxx.xxx.xxx.xxx.',
   'date': 'Дата и время должны иметь формат: dd.mm.yyyy.',
   'unique': 'Поле {{ field }} должно быть уникальным.',
   'password.confirmed': 'Поле: "пароля" и "подтверждение пароля" должны совпадать!',
-  'number': 'Поле {{ field }} должно быть числом.',
+  'number': 'В поле {{ field }} должно быть введено число.',
+  'withoutDecimals': 'В поле {{ field }} должно быть введено целое число.',
+  'positive': 'В поле {{ field }} должно быть введено положительное число.',
 
   // Error message for the username field
   'username.required': 'Please choose a username for your account',
