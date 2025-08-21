2224026423c8f35c0cb652f58a3a34faa1394236
Mikhail <bolotnikovms@yandex.ru>
2025-08-21 21:20:15 +0500

    refactor: Переработан интерфейс

--- app/domains/logs/interfaces/index.ts
+++ app/domains/logs/interfaces/index.ts
@@ -1,18 +1,21 @@
 import { IQueryParams } from "#interfaces/query_params"
-import { ELogActionType, ELogStatus } from "../enums/index.js"
+import { ELogActionType } from "../enums/index.js"
+import { TLogAction } from "../types/index.js"
 
 export interface INewLogRecord {
-  userId: number
-  actionType: ELogActionType
-  entityType: string
-  entityId: number | null
-  userIpAddress: string
-  additionalData: string | null
-  status: ELogStatus
-  errorMessage: string | null
+  userId: number | null
+  url?: string
+  method?: string
+  statusCode?: number | null
+  action: TLogAction,
+  errorType?: string
+  errorMessage?: string
+  model?: string | null
+  data: any
+  changes?: any
+  ipAddress?: string
 }
 
 export interface IQueryParamsLog extends IQueryParams {
   action: ELogActionType
-  status: ELogStatus
 }
