0b3ea3f5bafdb7c584a896b2d7a31901e9b8ea79
Mikhail <bolotnikovms@yandex.ru>
2025-08-22 18:34:02 +0500

    feat: Добавлен маршрут для полчения записи

--- start/routes/api/v1/logs.ts
+++ start/routes/api/v1/logs.ts
@@ -5,6 +5,7 @@ import router from '@adonisjs/core/services/router'
 export const logRoutes = router
   .group(() => {
     router.get('/', [LogsController, 'index'])
+    router.get('/:id', [LogsController, 'getLogInfo'])
   })
   .prefix('/logs')
   .use([middleware.auth()])
