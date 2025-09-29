bf8485c5c49416af4cefc9c409a82f74f49999af
Mikhail <bolotnikovms@yandex.ru>
2025-09-29 16:53:25 +0500

    feat: Добавлен интерфейс ICreateTelemechanicsDevice

--- app/domains/telemechanics_devices/interfaces/index.ts
+++ app/domains/telemechanics_devices/interfaces/index.ts
@@ -4,3 +4,7 @@ export interface ITelemechanicsDevice {
   headControllerId: number
   note?: string | null
 }
+
+export interface ICreateTelemechanicsDevice extends ITelemechanicsDevice {
+  userId: number
+}
