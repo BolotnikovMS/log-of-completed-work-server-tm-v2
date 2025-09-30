d19dceef7d070eabbe4f2efa5a5234690f477369
Mikhail <bolotnikovms@yandex.ru>
2025-09-30 16:49:13 +0500

    fix: Перемещение файла

--- app/dtos/substations/index.ts
+++ app/dtos/substations/index.ts
@@ -3,5 +3,6 @@ import SubstationChannelsDto from './substation_channels.js'
 import SubstationFileListDto from './substation_file_list.js'
 import SubstationInfoDto from './substation_info.js'
 import SubstationListDto from './substation_lists.js'
+import SubstationTelemechanicsDevicesDto from './substation_telemechanics_devices.js'
 
- export { SubstationListDto, SubstationDto, SubstationChannelsDto, SubstationFileListDto, SubstationInfoDto }
+export { SubstationChannelsDto, SubstationDto, SubstationFileListDto, SubstationInfoDto, SubstationListDto, SubstationTelemechanicsDevicesDto }
