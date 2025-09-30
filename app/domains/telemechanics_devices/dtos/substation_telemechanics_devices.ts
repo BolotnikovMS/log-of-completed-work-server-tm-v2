7ebd4a8a9b6b3c676b9eebeb96336c3ce37a3f08
Mikhail <bolotnikovms@yandex.ru>
2025-09-30 16:44:42 +0500

    feat: ДТО SubstationTelemechanicsDevicesDto Для использования при получении данных по объекту

---
+++ app/domains/telemechanics_devices/dtos/substation_telemechanics_devices.ts
@@ -0,0 +1,20 @@
+import TelemechanicsDevice from '#models/telemechanics_device'
+import { BaseModelDto } from '@adocasts.com/dto/base'
+
+export default class SubstationTelemechanicsDevicesDto extends BaseModelDto {
+  declare id: number
+  declare note?: string | null
+  declare type_kp: string | null
+  declare head_controller: string | null
+
+  constructor(telemechanicsDevice?: TelemechanicsDevice) {
+    super()
+
+    if (!telemechanicsDevice) return
+
+    this.id = telemechanicsDevice.id
+    this.note = telemechanicsDevice.note
+    this.type_kp = telemechanicsDevice.type_kp && telemechanicsDevice.type_kp.name
+    this.head_controller = telemechanicsDevice.head_controller && telemechanicsDevice.head_controller.name
+  }
+}
