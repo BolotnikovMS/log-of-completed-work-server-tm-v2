703b3f95a5b8fa1f9c774cfbba7bd481425c3d7a
Mikhail <bolotnikovms@yandex.ru>
2025-10-07 15:28:30 +0500

    feat: dto TelemechanicsDeviceListDto

---
+++ app/domains/telemechanics_devices/dtos/telemechanics_device_list.ts
@@ -0,0 +1,22 @@
+import TelemechanicsDevice from '#models/telemechanics_device'
+import { BaseModelDto } from '@adocasts.com/dto/base'
+
+export default class TelemechanicsDeviceListDto extends BaseModelDto {
+  declare id: number
+  declare note?: string | null
+  declare substation: string
+  declare type_kp: string
+  declare head_controller: string
+
+  constructor(telemechanicsDevice?: TelemechanicsDevice) {
+    super()
+
+    if (!telemechanicsDevice) return
+
+    this.id = telemechanicsDevice.id
+    this.note = telemechanicsDevice.note
+    this.substation = telemechanicsDevice.substation.fullNameSubstation
+    this.type_kp = telemechanicsDevice.type_kp.name
+    this.head_controller = telemechanicsDevice.head_controller.name
+  }
+}
