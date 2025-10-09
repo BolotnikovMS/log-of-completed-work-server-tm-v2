c5c0b19a7b086fdc6271f6ac00f1344720f68c2c
Mikhail <bolotnikovms@yandex.ru>
2025-10-09 16:27:59 +0500

    feat: Добавлена проверка проверка поля actualFirmwareVersion

--- app/validators/head_controller.ts
+++ app/validators/head_controller.ts
@@ -1,8 +1,9 @@
 import vine from '@vinejs/vine'
-import { text30 } from './fields_check.js'
+import { text150, text30 } from './fields_check.js'
 
 export const headControllerValidator = vine.compile(
   vine.object({
     name: text30.escape(),
+    actualFirmwareVersion: text150.escape().optional().nullable()
   })
 )
