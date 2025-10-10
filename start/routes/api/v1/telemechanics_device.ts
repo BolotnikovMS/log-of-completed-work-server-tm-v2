70457eebbe56c17106434b5ec984a1d98bde8e05
Mikhail <bolotnikovms@yandex.ru>
2025-10-10 15:42:18 +0500

    feat: Добавлен роут для получения инфы

--- start/routes/api/v1/telemechanics_device.ts
+++ start/routes/api/v1/telemechanics_device.ts
@@ -6,6 +6,7 @@ export const telemechanicsDevicesRoutes = router
   .group(() => {
     router.get('/', [TelemechanicsDevicesController, 'index'])
     router.get('/:id', [TelemechanicsDevicesController, 'getTelemechanicsDeviceById'])
+    router.get('/:id/info', [TelemechanicsDevicesController, 'getTelemechanicsDeviceInfoById'])
     router.post('/', [TelemechanicsDevicesController, 'store'])
     router.patch('/:id', [TelemechanicsDevicesController, 'update'])
     router.delete('/:id', [TelemechanicsDevicesController, 'destroy'])
