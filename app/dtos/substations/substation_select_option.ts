0cc05304ef0d329893d6b274aabb5b17966fb1cd
Mikhail <bolotnikovms@yandex.ru>
2025-10-15 15:42:29 +0500

    feat: SubstationSelectOptionDto

---
+++ app/dtos/substations/substation_select_option.ts
@@ -0,0 +1,16 @@
+import Substation from '#models/substation'
+import { BaseModelDto } from '@adocasts.com/dto/base'
+
+export default class SubstationSelectOptionDto extends BaseModelDto {
+  declare id: number
+  declare name: string
+
+  constructor(substation?: Substation) {
+    super()
+
+    if (!substation) return
+
+    this.id = substation.id
+    this.name = substation.fullNameSubstation ?? substation.name
+  }
+}
