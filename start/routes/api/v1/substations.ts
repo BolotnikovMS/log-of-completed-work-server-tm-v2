4ee8017edee31b3a65de2054c4c08b9f37d9d0c7
Mikhail <bolotnikovms@yandex.ru>
2025-10-15 15:39:41 +0500

    feat: Маршрут получения данных для select

--- start/routes/api/v1/substations.ts
+++ start/routes/api/v1/substations.ts
@@ -6,6 +6,7 @@ export const substationsRoutes = router
   .group(() => {
     router.get('/', [SubstationsController, 'index'])
     router.post('/', [SubstationsController, 'store'])
+    router.get('/select-options', [SubstationsController, 'getSubstationsForSelect'])
     router.get('/download-substations-excel', [SubstationsController, 'downloadSubstationsExcel'])
     router.get('/:id/info', [SubstationsController, 'getSubstationInfo'])
     router.get('/:id', [SubstationsController, 'getSubstation'])
