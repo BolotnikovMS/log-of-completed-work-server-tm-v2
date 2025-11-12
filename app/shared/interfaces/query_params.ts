adb3eccf4d80fe79d26c6ed55790204ef58fb46f
Mikhail <bolotnikovms@yandex.ru>
2025-11-12 17:05:37 +0500

    fix: Поля сделаны не обязательными

--- app/shared/interfaces/query_params.ts
+++ app/shared/interfaces/query_params.ts
@@ -2,27 +2,27 @@ import { OrderByEnums } from "#shared/enums/sort"
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
 export interface IQueryParams {
-  page: number
-  size: number
-  sort: string
-  order: OrderByEnums
-  search: string
-  active: string
-  limit: number
-  offset: number
-  substation: string
-  cleanUser: string
-  dateStart: string
-  dateEnd: string
-  executor: string
-  typeKp: string
-  headController: string
-  district: string
-  channelType: string
-  channelCategory: string
-  typeWork: string
-  inControl: string
-  objectType: string
+  page?: number
+  size?: number
+  sort?: string
+  order?: OrderByEnums
+  search?: string
+  active?: string
+  limit?: number
+  offset?: number
+  substation?: string
+  cleanUser?: string
+  dateStart?: string
+  dateEnd?: string
+  executor?: string
+  typeKp?: string
+  headController?: string
+  district?: string
+  channelType?: string
+  channelCategory?: string
+  typeWork?: string
+  inControl?: string
+  objectType?: string
 }
 
 export interface IQueryParams2 {
