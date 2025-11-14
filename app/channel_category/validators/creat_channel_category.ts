b4ade7e9a3111b3e2f9de0383ff1244b3b027d67
Mikhail <bolotnikovms@yandex.ru>
2025-11-14 16:45:05 +0500

    refactor: Разделены схемы валидации

--- app/channel_category/validators/creat_channel_category.ts
+++ app/channel_category/validators/creat_channel_category.ts
@@ -1,7 +1,7 @@
 import { text50 } from '#shared/validators/fields_check'
 import vine from '@vinejs/vine'
 
-export const channelCategoryValidator = vine.compile(
+export const createChannelCategoryValidator = vine.compile(
   vine.object({
     name: text50.escape(),
     shortName: text50.escape(),
---
+++ app/channel_category/validators/index.ts
@@ -0,0 +1,4 @@
+import { createChannelCategoryValidator } from './creat_channel_category.js'
+import { updateChannelCategoryValidator } from './update_channel_category.js'
+
+export { createChannelCategoryValidator, updateChannelCategoryValidator }
