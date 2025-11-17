a57d9526b14b17f91533b02e05b373009474614f
Mikhail <bolotnikovms@yandex.ru>
2025-11-17 16:52:14 +0500

    fix: Поправлены импорты

---
+++ app/channeling_equipment/interfaces/index.ts
@@ -0,0 +1,4 @@
+import type { CreateChannelingEquipment, UpdateChannelingEquipment } from './channeling_equipment.js'
+import type { ChannelEquipQueryParams } from './query_params_channeling_equipment.js'
+
+export type { ChannelEquipQueryParams, CreateChannelingEquipment, UpdateChannelingEquipment }
