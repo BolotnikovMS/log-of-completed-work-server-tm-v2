0504f8b3859423417b611b606aa7dbd83796df03
Mikhail <bolotnikovms@yandex.ru>
2025-11-17 16:58:39 +0500

    refactor: Переработана базовая схема

--- app/shared/validators/query_param.ts
+++ app/shared/validators/query_param.ts
@@ -1,12 +1,12 @@
 import { OrderByEnums } from '#shared/enums/sort'
 import vine from '@vinejs/vine'
 
-export const queryParamsValidator = vine.compile(
-  vine.object({
-    page: vine.number().positive().withoutDecimals().min(1).optional(),
-    limit: vine.number().positive().withoutDecimals().min(1).optional(),
-    sort: vine.string().optional(),
-    order: vine.enum(OrderByEnums).optional(),
-    search: vine.string().optional(),
-  })
-)
+export const baseQueryParamsSchema = {
+  page: vine.number().positive().withoutDecimals().min(1).optional(),
+  limit: vine.number().positive().withoutDecimals().min(1).optional(),
+  sort: vine.string().optional(),
+  order: vine.enum(OrderByEnums).optional(),
+  search: vine.string().optional(),
+}
+
+export const baseQueryParamsValidator = vine.compile(vine.object(baseQueryParamsSchema))
