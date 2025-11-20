bd4ebb6a284d4c50377fd089aa2919bf5d96cc04
Mikhail <bolotnikovms@yandex.ru>
2025-11-20 21:01:06 +0500

    refactor: Разделены схемы

---
+++ app/district/validators/index.ts
@@ -0,0 +1,4 @@
+import { createDistrictValidator } from './create_district.js'
+import { updateDistrictValidator } from './update_district.js'
+
+export { createDistrictValidator, updateDistrictValidator }
