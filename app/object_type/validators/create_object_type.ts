15fbeeeab3e3d9a3c5b5dd8b3eaf46a4f1c72224
Mikhail <bolotnikovms@yandex.ru>
2025-11-20 20:42:52 +0500

    refactor: Схемы разделены

--- app/object_type/validators/create_object_type.ts
+++ app/object_type/validators/create_object_type.ts
@@ -1,7 +1,7 @@
 import { text50 } from '#shared/validators/fields_check'
 import vine from '@vinejs/vine'
 
-export const objectTypeValidator = vine.compile(
+export const createObjectTypeValidator = vine.compile(
   vine.object({
     name: text50.escape(),
     shortName: text50.escape(),
