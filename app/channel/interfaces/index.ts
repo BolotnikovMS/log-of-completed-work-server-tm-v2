eacc3a79f58715a10c30f61b1b0c29ef38ccebce
Mikhail <bolotnikovms@yandex.ru>
2025-11-26 16:25:39 +0500

    feat: Файл с экспортами

---
+++ app/channel/interfaces/index.ts
@@ -0,0 +1,4 @@
+import type { CreateChannel, UpdateChannel } from './channel.js'
+import type { ChannelQueryParams } from './query_params_channels.js'
+
+export type { ChannelQueryParams, CreateChannel, UpdateChannel }
