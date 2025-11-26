724f27227df262d4ce84927c736ac36ff834e5a4
Mikhail <bolotnikovms@yandex.ru>
2025-11-26 16:24:40 +0500

    feat: Схема валидации параметров

---
+++ app/channel/validators/query_params_channel.ts
@@ -0,0 +1,12 @@
+import { noStrictNumberCheck } from '#shared/validators/fields_check'
+import { baseQueryParamsSchema } from '#shared/validators/index'
+import vine from '@vinejs/vine'
+
+const channelQueryParamsSchema = {
+  ...baseQueryParamsSchema,
+  substations: noStrictNumberCheck.optional(),
+  channelType: noStrictNumberCheck.optional(),
+  channelCategory: noStrictNumberCheck.optional(),
+}
+
+export const queryParamsChannelValidator = vine.compile(vine.object(channelQueryParamsSchema))
