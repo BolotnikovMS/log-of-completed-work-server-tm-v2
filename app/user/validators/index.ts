be8ec03fb0418206038738b2277e0a38efca7423
Mikhail <bolotnikovms@yandex.ru>
2025-12-02 17:01:36 +0500

    feat: Добавлена схема проверки параметров

--- app/user/validators/index.ts
+++ app/user/validators/index.ts
@@ -2,6 +2,7 @@ import { blockUserAccountValidator } from './block_user_account.js'
 import { changePasswordValidator } from './change_password.js'
 import { changeUserRole } from './change_user_role.js'
 import { loginValidator } from './login.js'
+import { queryParamsUsersValidator } from './query_params_user.js'
 import { registerValidator } from './registrer.js'
 
-export { blockUserAccountValidator, changePasswordValidator, changeUserRole, loginValidator, registerValidator }
+export { blockUserAccountValidator, changePasswordValidator, changeUserRole, loginValidator, queryParamsUsersValidator, registerValidator }
