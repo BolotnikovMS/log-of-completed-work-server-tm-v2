bb16957e6042a9849a085e0bb034acbb46fbdf7b
Mikhail <bolotnikovms@yandex.ru>
2025-12-02 17:03:04 +0500

    feat: Экспорт

---
+++ app/user/interfaces/index.ts
@@ -0,0 +1,4 @@
+import type { UserQueryParams } from './query_params_users.js'
+import type { BlockUserAccount, ChangeUserPassword, ChangeUserRole, CreateUser } from './user.js'
+
+export type { BlockUserAccount, ChangeUserPassword, ChangeUserRole, CreateUser, UserQueryParams }
