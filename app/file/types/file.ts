da956c34f4f3c74a0cad8fd417c26002d433c5f0
Mikhail <bolotnikovms@yandex.ru>
2025-12-04 20:41:03 +0500

    fix: Переименован файл. Изменено наименование файла. Добавлены типы на основе схем валидации.

--- app/file/types/file.ts
+++
@@ -1,4 +0,0 @@
-import type { uploadFileSubstationValidator } from "#file/validators/index"
-import type { Infer } from "@vinejs/vine/types"
-
-export type FileSubstation = Infer<typeof uploadFileSubstationValidator> & { userId: number }
---
+++ app/file/types/index.ts
@@ -0,0 +1,6 @@
+import type { fileSubstationKeyValidator, uploadFileSubstationValidator } from '#file/validators/index'
+import type { Infer } from '@vinejs/vine/types'
+
+export type FileSubstation = Infer<typeof uploadFileSubstationValidator> & { userId: number }
+
+export type FileSubstationKey = Infer<typeof fileSubstationKeyValidator>
