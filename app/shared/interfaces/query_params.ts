1e6c227fbfacd1741feb6b5d2e1fad9561fdd698
Mikhail <bolotnikovms@yandex.ru>
2025-12-09 16:51:56 +0500

    fix: Удален IQueryParams интерфейс

--- app/shared/interfaces/query_params.ts
+++ app/shared/interfaces/query_params.ts
@@ -1,33 +1,9 @@
-import { OrderByEnums } from "#shared/enums/sort"
+import { OrderByEnums } from '#shared/enums/sort'
 
-// eslint-disable-next-line @typescript-eslint/naming-convention
 export interface BaseQueryParams {
   page?: number
   limit?: number
   search?: string
   sort?: string
   order?: OrderByEnums
-}
-export interface IQueryParams {
-  page?: number
-  size?: number
-  sort?: string
-  order?: OrderByEnums
-  search?: string
-  active?: string
-  limit?: number
-  offset?: number
-  substation?: string
-  cleanUser?: string
-  dateStart?: string
-  dateEnd?: string
-  executor?: string
-  typeKp?: string
-  headController?: string
-  district?: string
-  channelType?: string
-  channelCategory?: string
-  typeWork?: string
-  inControl?: string
-  objectType?: string
 }
