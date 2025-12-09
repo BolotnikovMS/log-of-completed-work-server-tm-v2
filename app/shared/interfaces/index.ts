b7f769912019741ba6f9746fc0125b7e3d60195f
Mikhail <bolotnikovms@yandex.ru>
2025-12-09 16:50:43 +0500

    fix: Правки Поправлены наименования интерфейсов, экспорты.

--- app/shared/interfaces/index.ts
+++ app/shared/interfaces/index.ts
@@ -1,4 +1,4 @@
-import type { IParams } from './params.js'
-import type { BaseQueryParams, IQueryParams } from './query_params.js'
+import type { Params } from './params.js'
+import type { BaseQueryParams } from './query_params.js'
 
-export type { BaseQueryParams, IParams, IQueryParams }
+export type { BaseQueryParams, Params }
