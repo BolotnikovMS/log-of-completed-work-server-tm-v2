3ddb51e17b477afdfb8661953ba5649b0e01bd03
Mikhail <bolotnikovms@yandex.ru>
2025-12-18 18:29:34 +0500

    fix: Тип параметров выведен из схемы валидации

--- app/substation/interfaces/index.ts
+++ app/substation/interfaces/index.ts
@@ -1,4 +1,3 @@
 import type { CSVSubstationKeyRow, ErrorParseCSVSubstationKey, ProcessCSVFile, ResultParseCSVSubstationKey } from './import_csv_substations_key.js'
-import type { SubstationQueryParams } from './qury_params_substations.js'
 
-export type { CSVSubstationKeyRow, ErrorParseCSVSubstationKey, ProcessCSVFile, ResultParseCSVSubstationKey, SubstationQueryParams }
+export type { CSVSubstationKeyRow, ErrorParseCSVSubstationKey, ProcessCSVFile, ResultParseCSVSubstationKey }
---
+++ app/substation/types/index.ts
@@ -0,0 +1,4 @@
+import type { QueryParamsSubstation } from './qury_params_substations.js'
+import type { CreateSubstation, KeyDefectSubstation, UpdateNoteSubstation, UpdateSubstation } from './substation.js'
+
+export type { CreateSubstation, KeyDefectSubstation, QueryParamsSubstation, UpdateNoteSubstation, UpdateSubstation }
