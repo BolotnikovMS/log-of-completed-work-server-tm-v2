364e2207ea63a3c7460456a206396416d2d2778f
Mikhail <bolotnikovms@yandex.ru>
2025-12-19 16:24:40 +0500

    fix: Убран импорт

--- app/log/interfaces/index.ts
+++ app/log/interfaces/index.ts
@@ -1,4 +1,3 @@
 import type { CreateLogRecord } from './log.js'
-import type { QueryParamsLog } from './query_params_log.js'
 
-export type { CreateLogRecord, QueryParamsLog }
+export type { CreateLogRecord }
--- app/log/interfaces/query_params_log.ts
+++
@@ -1,6 +0,0 @@
-import type { LogActionType } from '#log/enums/index'
-import type { BaseQueryParams } from '#shared/interfaces/query_params'
-
-export interface QueryParamsLog extends BaseQueryParams {
-  action?: LogActionType
-}
