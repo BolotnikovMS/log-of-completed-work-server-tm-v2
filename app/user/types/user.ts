d2de8d4ab1f493c050dd5b8460cffd72d5eeef2d
Mikhail <bolotnikovms@yandex.ru>
2025-12-19 16:10:35 +0500

    fix: Вывод типа из сземы валидации

--- app/user/types/user.ts
+++ app/user/types/user.ts
@@ -1,16 +1,10 @@
-import type { registerValidator } from '#user/validators/registrer'
+import type { blockUserAccountValidator, changePasswordValidator, changeUserRole, registerValidator } from '#user/validators/index'
 import type { Infer } from '@vinejs/vine/types'
 
 export type CreateUser = Infer<typeof registerValidator>
 
-export type ChangeUserPassword = {
-  password: string
-}
+export type ChangeUserPassword = Infer<typeof changePasswordValidator>
 
-export type BlockUserAccount = {
-  active: boolean
-}
+export type BlockUserAccount = Infer<typeof blockUserAccountValidator>
 
-export type ChangeUserRole = {
-  roleId: number
-}
+export type ChangeUserRole = Infer<typeof changeUserRole>
