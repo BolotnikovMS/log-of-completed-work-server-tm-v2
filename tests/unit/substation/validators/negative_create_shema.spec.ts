baa88b48c9d938d1533ac608a34f98cc127e239a
Mikhail <bolotnikovms@yandex.ru>
2025-12-25 15:23:34 +0500

    fix: Обновлены тестовые данные

--- tests/unit/substation/validators/negative_create_shema.spec.ts
+++ tests/unit/substation/validators/negative_create_shema.spec.ts
@@ -14,23 +14,162 @@ test.group('⛔️ Негативные тесты. Тесты для прове
   }
   const incorrectData = [
     // districtId
-    { active: true, districtId: '1', voltageClassesId: voltageClasses.id, objectTypeId: objectType.id, name: 'Test', rdu: false, descrTest: 'districtId - Не число (строка)', errMessage: 'В поле districtId должно быть введено число.' },
-    { active: true, districtId: true, voltageClassesId: voltageClasses.id, objectTypeId: objectType.id, name: 'Test', rdu: false, descrTest: 'districtId - Не число (булево)', errMessage: 'В поле districtId должно быть введено число.' },
-    { active: true, districtId: null, voltageClassesId: voltageClasses.id, objectTypeId: objectType.id, name: 'Test', rdu: false, descrTest: 'districtId - Не число (null)', errMessage: 'Поле является обязательным.' },
-    { active: true, districtId: {}, voltageClassesId: voltageClasses.id, objectTypeId: objectType.id, name: 'Test', rdu: false, descrTest: 'districtId - Не число (объект)', errMessage: 'В поле districtId должно быть введено число.' },
-    { active: true, districtId: -4, voltageClassesId: voltageClasses.id, objectTypeId: objectType.id, name: 'Test', rdu: false, descrTest: 'districtId - Отрицательное', errMessage: 'В поле districtId должно быть введено положительное число.' },
-    { active: true, districtId: 1.4, voltageClassesId: voltageClasses.id, objectTypeId: objectType.id, name: 'Test', rdu: false, descrTest: 'districtId - Дробное', errMessage: 'В поле districtId должно быть введено целое число.' },
-    { active: true, districtId: 0, voltageClassesId: voltageClasses.id, objectTypeId: objectType.id, name: 'Test', rdu: false, descrTest: 'districtId - Ноль', errMessage: 'Значение поля districtId должно быть не меньше 1.' },
-    { active: true, districtId: district.id + 1, voltageClassesId: voltageClasses.id, objectTypeId: objectType.id, name: 'Test', rdu: false, descrTest: 'districtId - Не существует в БД', errMessage: 'Выбранный districtId является недопустимым.' },
+    {
+      active: true,
+      districtId: '1',
+      voltageClassesId: voltageClasses.id,
+      objectTypeId: objectType.id,
+      name: 'Test',
+      rdu: false,
+      descrTest: 'districtId - Не число (строка)',
+      errMessage: 'В поле districtId должно быть введено число.'
+    },
+    {
+      active: true,
+      districtId: true,
+      voltageClassesId: voltageClasses.id,
+      objectTypeId: objectType.id,
+      name: 'Test',
+      rdu: false,
+      descrTest: 'districtId - Не число (булево)',
+      errMessage: 'В поле districtId должно быть введено число.'
+    },
+    {
+      active: true,
+      districtId: null,
+      voltageClassesId: voltageClasses.id,
+      objectTypeId: objectType.id,
+      name: 'Test', rdu: false,
+      descrTest: 'districtId - Не число (null)',
+      errMessage: 'Поле является обязательным.'
+    },
+    {
+      active: true,
+      districtId: {},
+      voltageClassesId: voltageClasses.id,
+      objectTypeId: objectType.id,
+      name: 'Test',
+      rdu: false,
+      descrTest: 'districtId - Не число (объект)',
+      errMessage: 'В поле districtId должно быть введено число.'
+    },
+    {
+      active: true,
+      districtId: -4,
+      voltageClassesId: voltageClasses.id,
+      objectTypeId: objectType.id,
+      name: 'Test',
+      rdu: false,
+      descrTest: 'districtId - Отрицательное',
+      errMessage: 'В поле districtId должно быть введено положительное число.'
+    },
+    {
+      active: true,
+      districtId: 1.4,
+      voltageClassesId: voltageClasses.id,
+      objectTypeId: objectType.id,
+      name: 'Test',
+      rdu: false,
+      descrTest: 'districtId - Дробное',
+      errMessage: 'В поле districtId должно быть введено целое число.'
+    },
+    {
+      active: true,
+      districtId: 0,
+      voltageClassesId: voltageClasses.id,
+      objectTypeId: objectType.id,
+      name: 'Test',
+      rdu: false,
+      descrTest: 'districtId - Ноль',
+      errMessage: 'Значение поля districtId должно быть не меньше 1.'
+    },
+    {
+      active: true,
+      districtId: district.id + 1,
+      voltageClassesId: voltageClasses.id,
+      objectTypeId: objectType.id,
+      name: 'Test',
+      rdu: false, descrTest: 'districtId - Не существует в БД',
+      errMessage: 'Выбранный districtId является недопустимым.'
+    },
     //voltageClassesId
-    { active: true, districtId: district.id, voltageClassesId: '1', objectTypeId: objectType.id, name: 'Test', rdu: false, descrTest: 'districtId - Не число (строка)', errMessage: 'В поле voltageClassesId должно быть введено число.' },
-    { active: true, districtId: district.id, voltageClassesId: true, objectTypeId: objectType.id, name: 'Test', rdu: false, descrTest: 'districtId - Не число (булево)', errMessage: 'В поле voltageClassesId должно быть введено число.' },
-    { active: true, districtId: district.id, voltageClassesId: null, objectTypeId: objectType.id, name: 'Test', rdu: false, descrTest: 'districtId - Не число (null)', errMessage: 'Поле является обязательным.' },
-    { active: true, districtId: district.id, voltageClassesId: {}, objectTypeId: objectType.id, name: 'Test', rdu: false, descrTest: 'districtId - Не число (объект)', errMessage: 'В поле voltageClassesId должно быть введено число.' },
-    { active: true, districtId: district.id, voltageClassesId: -3, objectTypeId: objectType.id, name: 'Test', rdu: false, descrTest: 'districtId - Отрицательное', errMessage: 'В поле voltageClassesId должно быть введено положительное число.' },
-    { active: true, districtId: district.id, voltageClassesId: 1.2, objectTypeId: objectType.id, name: 'Test', rdu: false, descrTest: 'districtId - Дробное', errMessage: 'В поле voltageClassesId должно быть введено целое число.' },
-    { active: true, districtId: district.id, voltageClassesId: 0, objectTypeId: objectType.id, name: 'Test', rdu: false, descrTest: 'districtId - Ноль', errMessage: 'Значение поля voltageClassesId должно быть не меньше 1.' },
-    { active: true, districtId: district.id, voltageClassesId: voltageClasses.id + 1, objectTypeId: objectType.id, name: 'Test', rdu: false, descrTest: 'districtId - Не существует в БД', errMessage: 'Выбранный voltageClassesId является недопустимым.' },
+    {
+      active: true,
+      districtId: district.id,
+      voltageClassesId: '1',
+      objectTypeId: objectType.id,
+      name: 'Test', rdu: false,
+      descrTest: 'voltageClassesId - Не число (строка)',
+      errMessage: 'В поле voltageClassesId должно быть введено число.'
+    },
+    {
+      active: true,
+      districtId: district.id,
+      voltageClassesId: true,
+      objectTypeId: objectType.id,
+      name: 'Test',
+      rdu: false,
+      descrTest: 'voltageClassesId - Не число (булево)',
+      errMessage: 'В поле voltageClassesId должно быть введено число.'
+    },
+    {
+      active: true,
+      districtId: district.id,
+      voltageClassesId: null,
+      objectTypeId: objectType.id,
+      name: 'Test',
+      rdu: false,
+      descrTest: 'voltageClassesId - Не число (null)',
+      errMessage: 'Поле является обязательным.'
+    },
+    {
+      active: true,
+      districtId: district.id,
+      voltageClassesId: {},
+      objectTypeId: objectType.id,
+      name: 'Test',
+      rdu: false,
+      descrTest: 'voltageClassesId - Не число (объект)',
+      errMessage: 'В поле voltageClassesId должно быть введено число.'
+    },
+    {
+      active: true,
+      districtId: district.id,
+      voltageClassesId: -3,
+      objectTypeId: objectType.id,
+      name: 'Test',
+      rdu: false,
+      descrTest: 'voltageClassesId - Отрицательное',
+      errMessage: 'В поле voltageClassesId должно быть введено положительное число.'
+    },
+    {
+      active: true,
+      districtId: district.id,
+      voltageClassesId: 1.2,
+      objectTypeId: objectType.id,
+      name: 'Test',
+      rdu: false,
+      descrTest: 'voltageClassesId - Дробное',
+      errMessage: 'В поле voltageClassesId должно быть введено целое число.'
+    },
+    {
+      active: true,
+      districtId: district.id,
+      voltageClassesId: 0,
+      objectTypeId: objectType.id,
+      name: 'Test',
+      rdu: false,
+      descrTest: 'voltageClassesId - Ноль', errMessage: 'Значение поля voltageClassesId должно быть не меньше 1.'
+    },
+    {
+      active: true,
+      districtId: district.id,
+      voltageClassesId: voltageClasses.id + 1,
+      objectTypeId: objectType.id,
+      name: 'Test',
+      rdu: false, descrTest: 'voltageClassesId - Не существует в БД',
+      errMessage: 'Выбранный voltageClassesId является недопустимым.'
+    },
   ]
 
   test('{$i} - Проверка: {descrTest}.')
