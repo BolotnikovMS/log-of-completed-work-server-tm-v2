a18a3e471a1588acc8cf0192fe98ca2e181da5ff
Mikhail <bolotnikovms@yandex.ru>
2025-12-30 18:41:59 +0500

    feat: Тесты негативных проверок

--- tests/unit/substation/validators/negative_update_shema.spec.ts
+++ tests/unit/substation/validators/negative_update_shema.spec.ts
@@ -1,6 +1,14 @@
+import { updateSubstationValidator } from '#substation/validators/update_substation'
 import { test } from '@japa/runner'
+import { incorrectUpdateTestData } from './test_data.js'
 
-test.group('Substation validators negative update shema', () => {
-  test('example test', async ({ assert }) => {
-  })
+test.group('⛔️ Негативные тесты. Тесты для проверки функционала "Substation', () => {
+  test('{$i} - Проверка: {descrTest}.')
+    .with(incorrectUpdateTestData)
+    .run(async ({ assert }, testItem) => {
+      const output = await updateSubstationValidator.tryValidate(testItem)
+      const data = output[0]?.messages
+
+      assert.containsSubset(data, [{ message: testItem.errMessage }])
+    })
 })
