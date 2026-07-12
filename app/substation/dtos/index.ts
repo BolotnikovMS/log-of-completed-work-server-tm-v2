4bc8aa9374e6a6bfc0fc7d522607b88cd3b7280d
Mikhail <bolotnikovms@yandex.ru>
2026-07-12 15:20:50 +0500

    feat: Добавлен экспорт

--- app/substation/dtos/index.ts
+++ app/substation/dtos/index.ts
@@ -2,10 +2,11 @@ import SubstationDto from './substation.js'
 import SubstationChannelsDto from './substation_channels.js'
 import SubstationFileListDto from './substation_file_list.js'
 import SubstationInfoDto from './substation_info.js'
+import SubstationKeyDefectDto from './substation_key_defect.js'
 import SubstationListTelemechanicsDevicesDto from './substation_list_telemechanics_devices.js'
 import SubstationListDto from './substation_lists.js'
 import SubstationNoteDto from './substation_note.js'
 import SubstationSelectOptionDto from './substation_select_option.js'
 import SubstationTelemechanicsDevicesDto from './substation_telemechanics_devices.js'
 
-export { SubstationChannelsDto, SubstationDto, SubstationFileListDto, SubstationInfoDto, SubstationListDto, SubstationListTelemechanicsDevicesDto, SubstationNoteDto, SubstationSelectOptionDto, SubstationTelemechanicsDevicesDto }
+export { SubstationChannelsDto, SubstationDto, SubstationFileListDto, SubstationInfoDto, SubstationKeyDefectDto, SubstationListDto, SubstationListTelemechanicsDevicesDto, SubstationNoteDto, SubstationSelectOptionDto, SubstationTelemechanicsDevicesDto }
