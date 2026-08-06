353f9a95c51759b6b579c536de0f52661696e439
Mikhail <bolotnikovms@yandex.ru>
2026-08-06 17:00:04 +0500

    feat: Добавлен тип

--- app/file/types/file.ts
+++ app/file/types/file.ts
@@ -1,4 +1,4 @@
-import type { fileSubstationKeyValidator, uploadFileSubstationValidator } from '#file/validators/index'
+import type { fileSubstationKeyValidator, queryParamsFileValidation, uploadFileSubstationValidator } from '#file/validators/index'
 import type { Infer } from '@vinejs/vine/types'
 
 export type FileSubstation = Infer<typeof uploadFileSubstationValidator> & { userId: number }
@@ -8,3 +8,5 @@ export type FileSubstationKey = Infer<typeof fileSubstationKeyValidator>
 export type UpdFileName = {
   clientName: string
 }
+
+export type QueryParamsFile = Infer<typeof queryParamsFileValidation>
