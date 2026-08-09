df4d82d29527df6bf4bbd7d256994d8bccea4746
Mikhail <bolotnikovms@yandex.ru>
2026-08-09 17:22:08 +0500

    fix: Убран dto

--- app/substation/dtos/index.ts
+++ app/substation/dtos/index.ts
@@ -1,6 +1,5 @@
 import SubstationDto from './substation.js'
 import SubstationChannelsDto from './substation_channels.js'
-import SubstationFileListDto from './substation_file_list.js'
 import SubstationInfoDto from './substation_info.js'
 import SubstationKeyDefectDto from './substation_key_defect.js'
 import SubstationListTelemechanicsDevicesDto from './substation_list_telemechanics_devices.js'
@@ -9,4 +8,4 @@ import SubstationNoteDto from './substation_note.js'
 import SubstationSelectOptionDto from './substation_select_option.js'
 import SubstationTelemechanicsDevicesDto from './substation_telemechanics_devices.js'
 
-export { SubstationChannelsDto, SubstationDto, SubstationFileListDto, SubstationInfoDto, SubstationKeyDefectDto, SubstationListDto, SubstationListTelemechanicsDevicesDto, SubstationNoteDto, SubstationSelectOptionDto, SubstationTelemechanicsDevicesDto }
+export { SubstationChannelsDto, SubstationDto, SubstationInfoDto, SubstationKeyDefectDto, SubstationListDto, SubstationListTelemechanicsDevicesDto, SubstationNoteDto, SubstationSelectOptionDto, SubstationTelemechanicsDevicesDto }
