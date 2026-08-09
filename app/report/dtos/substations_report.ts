969b8a5ef8b1a9a8de500750c82fe5bf96985a3d
Mikhail <bolotnikovms@yandex.ru>
2026-08-09 17:22:20 +0500

    fix: Убраны поля

--- app/report/dtos/substations_report.ts
+++ app/report/dtos/substations_report.ts
@@ -1,5 +1,4 @@
 import { ChannelInfoDto } from '#channel/dtos/index'
-import { SubstationFileListDto } from '#substation/dtos/index'
 import Substation from '#substation/models/substation'
 import { BaseModelDto } from '@adocasts.com/dto/base'
 
@@ -12,9 +11,6 @@ export default class SubstationsReportDto extends BaseModelDto {
   declare note: string | null
   declare fullNameSubstation: string
   declare district: string | null
-  declare files_photos_ps: SubstationFileListDto[]
-  declare files_backups: SubstationFileListDto[]
-  declare other_files: SubstationFileListDto[]
   declare channels: ChannelInfoDto[]
   declare object_type: string | null
 
@@ -31,9 +27,6 @@ export default class SubstationsReportDto extends BaseModelDto {
     this.note = substation.note
     this.fullNameSubstation = substation.fullNameSubstation
     this.district = substation.district?.name
-    this.files_photos_ps = SubstationFileListDto.fromArray(substation.files_photos_ps)
-    this.files_backups = SubstationFileListDto.fromArray(substation.files_backups)
-    this.other_files = SubstationFileListDto.fromArray(substation.other_files)
     this.channels = ChannelInfoDto.fromArray(substation.channels)
     this.object_type = substation.object_type?.shortName
   }
