ce750739563a39353e0366a6474beefd029e6b7a
Mikhail <bolotnikovms@yandex.ru>
2026-08-23 15:32:09 +0500

    fix: Добавлен экспорт

--- app/shared/helpers/index.ts
+++ app/shared/helpers/index.ts
@@ -1,8 +1,9 @@
 import { accessErrorMessages } from './access_error_messages.js'
 import { getFileNameWithoutExtension } from './get_file_name_without_extension.js'
 import { logGeneration } from './logs.js'
+import { normalizeToNumberArray } from './normalize_to_array.js'
 import { randomStr } from './random_str.js'
 import { replacementEscapeSymbols } from './replacement_escape_symbols.js'
 import { transliterate } from './transliterate.js'
 
-export { accessErrorMessages, getFileNameWithoutExtension, logGeneration, randomStr, replacementEscapeSymbols, transliterate }
+export { accessErrorMessages, getFileNameWithoutExtension, logGeneration, normalizeToNumberArray, randomStr, replacementEscapeSymbols, transliterate }
