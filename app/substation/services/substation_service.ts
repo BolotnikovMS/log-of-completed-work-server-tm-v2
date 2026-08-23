2e984dbae4198e3886b7825851b4941723019c2e
Mikhail <bolotnikovms@yandex.ru>
2026-08-23 15:37:36 +0500

    refactor: Пересмотрена фильтрация

--- app/substation/services/substation_service.ts
+++ app/substation/services/substation_service.ts
@@ -1,3 +1,4 @@
+import { normalizeToNumberArray } from '#shared/helpers/index'
 import Substation from '#substation/models/substation'
 import type { CreateSubstation, KeyDefectSubstation, QueryParamsSubstation, UpdateNoteSubstation, UpdateSubstation } from '#substation/types/index'
 import type { ModelPaginatorContract } from '@adonisjs/lucid/types/model'
@@ -9,29 +10,24 @@ export default class SubstationService {
   ): Promise<ModelPaginatorContract<Substation>> {
     // const { sort = 'name', order = 'asc', page, limit, search, district, channelType, channelCategory, objectType, typeKp, headController } = filters
     const districtValue = districtId || filters?.district
+    const typeKpIds = normalizeToNumberArray(filters?.typeKp)
+    const headControllerIds = normalizeToNumberArray(filters?.headController)
+
     const substations = await Substation.query()
       .if(filters?.sort && filters?.order, (query) => query.orderBy(filters?.sort!, filters?.order!))
       .if(districtValue, (query) => query.where('district_id', '=', districtValue!))
       .if(filters?.search, (query) => query.whereLike('nameSearch', `%${filters?.search}%`))
       .if(filters?.objectType, (query) => query.where('object_type_id', '=', filters?.objectType!))
-      .if(filters?.channelType || filters?.channelCategory, query => {
+      .if(filters?.channelType || filters?.channelCategory, (query) => {
         query.whereHas('channels', query => {
-          if (filters?.channelType) {
-            query.where('channelTypeId', '=', filters?.channelType)
-          }
-          if (filters?.channelCategory) {
-            query.where('channelCategoryId', '=', filters?.channelCategory)
-          }
+          if (filters?.channelType) query.where('channelTypeId', '=', filters?.channelType)
+          if (filters?.channelCategory) query.where('channelCategoryId', '=', filters?.channelCategory)
         })
       })
-      .if(filters?.typeKp || filters?.headController, query => {
-        query.whereHas('telemechanics_device', query => {
-          if (filters?.typeKp) {
-            query.where('typeKpId', '=', filters?.typeKp)
-          }
-          if (filters?.headController) {
-            query.where('headControllerId', '=', filters?.headController)
-          }
+      .if(typeKpIds?.length || headControllerIds?.length, (query) => {
+        query.whereHas('telemechanics_device', (query) => {
+          if (typeKpIds?.length) query.whereIn('typeKpId', typeKpIds)
+          if (headControllerIds?.length) query.whereIn('headControllerId', headControllerIds)
         })
       })
       .preload('voltage_class', query => query.select('id', 'name'))
@@ -46,17 +42,13 @@ export default class SubstationService {
       })
       .preload('telemechanics_device', query => {
         query
-          .if(filters?.typeKp || filters?.headController, query => {
-            if (filters?.typeKp) {
-              query.where('typeKpId', '=', filters?.typeKp)
-            }
-            if (filters?.headController) {
-              query.where('headControllerId', '=', filters?.headController)
-            }
-          })
           .select('id', 'typeKpId', 'headControllerId', 'controllerFirmwareVersion', 'note')
           .preload('type_kp', query => query.select('id', 'name'))
           .preload('head_controller', query => query.select('id', 'name'))
+          .if(typeKpIds?.length || headControllerIds?.length, query => {
+            if (typeKpIds?.length) query.whereIn('typeKpId', typeKpIds)
+            if (headControllerIds?.length) query.whereIn('headControllerId', headControllerIds)
+          })
       })
       .paginate(filters?.page!, filters?.limit)
 
