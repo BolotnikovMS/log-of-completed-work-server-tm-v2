119ed7b1b7b739eb283dc948cc58389fd2c2ec62
Mikhail <bolotnikovms@yandex.ru>
2026-08-23 15:33:06 +0500

    fix: Внесены изменения

--- app/substation/validators/query_params_substations.ts
+++ app/substation/validators/query_params_substations.ts
@@ -1,3 +1,4 @@
+import { numberOrNumbersRule } from '#shared/rules/number_or_numbers'
 import { noStrictNumberCheck } from '#shared/validators/fields_check'
 import { baseQueryParamsSchema } from '#shared/validators/query_param'
 import vine from '@vinejs/vine'
@@ -8,8 +9,10 @@ const substationsQueryParamsSchema = {
   channelType: noStrictNumberCheck.optional(),
   channelCategory: noStrictNumberCheck.optional(),
   objectType: noStrictNumberCheck.optional(),
-  typeKp: noStrictNumberCheck.optional(),
-  headController: noStrictNumberCheck.optional()
+  // typeKp: noStrictNumberCheck.optional(),
+  // headController: noStrictNumberCheck.optional()
+  typeKp: vine.any().optional().use(numberOrNumbersRule({})),
+  headController: vine.any().optional().use(numberOrNumbersRule({}))
 }
 
 export const queryParamsSubstationsValidator = vine.compile(vine.object(substationsQueryParamsSchema))
